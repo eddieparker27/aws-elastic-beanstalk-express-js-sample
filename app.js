c0b30b1570da373ee3695c4ad1b52d6348e14bcd
Eddie Parker <eddieparker27@gmail.com>
2021-08-22 11:27:16 +0100

    Added floorboards

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Hannah!'));
+app.use(express.static('public'));
+
+app.get('/', (req, res) => res.send('<html><head><title>Testing Title</title></head><body><h1>Testing</h1><img src="images/floorboards_01.png" alt="Floorboards" width="500" height="600"></body></html>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);

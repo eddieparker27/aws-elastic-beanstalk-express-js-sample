b8101599851bed1006e2cc2b3652803b8a9808e5
Eddie Parker <eddieparker27@gmail.com>
2021-08-22 11:47:23 +0100

    Testing review stage

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 8080;
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => res.send('<html><head><title>Testing Title</title></head><body><h1>Testing</h1><img src="images/floorboards_01.png" alt="Floorboards" width="500" height="600"></body></html>'));
+app.get('/', (req, res) => res.send('<html><head><title>Testing Title</title></head><body><h1>Testing 2</h1><img src="images/floorboards_01.png" alt="Floorboards" width="500" height="600"></body></html>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
